18a96435c00ad1b8d7d5b51cc914636eaeb2a1db
Anderson Malaquias Da Silva <112710538+AndersonMSilva91@users.noreply.github.com>
2022-09-15 15:29:14 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-const r = require('./result');
-const weight = prompt('Insert your weight: ');
-const height = prompt('Insirt your height: ');
-const h = Number(height);
-const w = Number(weight);
-const finalweight = r(w,h)
-console.log(`your imc is: ${finalweight}`);
